1547474dec51ec9e292bf779338d0a57596dede5
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-02-20 19:01:57 +0200

    make the pages dynamic

--- index.js
+++ index.js
@@ -20,22 +20,50 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 //Server
+const replaceTemplate = (temp, product) => {
+	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+	if (!product.organic)
+		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+	return output;
+};
+
 const server = http.createServer((req, res) => {
-	const pathName = req.url;
+	const { query, pathname } = url.parse(req.url, true);
 
 	//OVERVIEW PAGE
-	if (pathName === '/' || pathName === '/overview') {
+	if (pathname === '/' || pathname === '/overview') {
 		res.writeHead(200, {
 			'Content-type': 'text/html',
 		});
-		res.end(tempOverview);
+
+		const cardsHtml = dataObj
+			.map((el) => replaceTemplate(tempCard, el))
+			.join('');
+
+		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+		res.end(output);
 
 		//PRODUCT PAGE
-	} else if (pathName === '/product') {
-		res.end('This is the Product');
+	} else if (pathname === '/product') {
+		res.writeHead(200, {
+			'Content-type': 'text/html',
+		});
+
+		const product = dataObj[query.id];
+		const output = replaceTemplate(tempProduct, product);
+
+		res.end(output);
 
 		//API
-	} else if (pathName === '/api') {
+	} else if (pathname === '/api') {
 		res.writeHead(200, {
 			'Content-type': 'application/json',
 		});
